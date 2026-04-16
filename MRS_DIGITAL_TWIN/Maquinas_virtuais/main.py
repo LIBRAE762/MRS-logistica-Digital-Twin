4598a4477e63f9685cbbabaa969c8d6e03c0fc44
LIBRAE <137963255+LIBRAE762@users.noreply.github.com>
2026-04-15 22:00:38 -0300

    Problema na geração de logs consertado e Lucro liquido adicionado

--- MRS_DIGITAL_TWIN/Maquinas_virtuais/main.py
+++ MRS_DIGITAL_TWIN/Maquinas_virtuais/main.py
@@ -12,7 +12,7 @@ trem2 = Trem(1420, "inter-modal", 5000, 70)
 
 # Carregando trens
 trem1.carregar(8000)
-trem2.carregar(2500)
+trem2.carregar(5000)
 
 # Iniciando viagens
 trem1.iniciar_viagem(rota1)
@@ -26,8 +26,10 @@ simulador.rodar()
 print("\n===== Resumo Final dos Trens =====")
 for trem in simulador.trens:
 	receita = trem.calcular_receita()
-	custo = trem.calcular_custo(trem.horas_viagem, atraso=0)
+	custo = trem.calcular_custo(trem.horas_viagem, trem.atraso_total)
+	lucroL = trem.calcular_lucrol(receita, custo)
 	print(f"Trem {trem.id_trem} - carga: {trem.carga_atual} t, distancia: {trem.rota.distancia_km} km")
 	print(f"  horas viagem: {trem.horas_viagem} h")
 	print(f"  receita: R$ {receita:.2f}")
-	print(f"  custo estimado: R$ {custo:.2f}\n")
+	print(f"  custo estimado: R$ {custo:.2f}")
+	print(f"  lucro liquido: R$ {lucroL:.2f}\n")
